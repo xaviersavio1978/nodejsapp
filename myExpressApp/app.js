b9621fe52081ebe3a69f28c007b1e843eacd6b19
Xavier Fernandes <xfdes28@hotmail.com>
2025-12-25 15:48:47 +1300

    update yaml pipeline

--- myExpressApp/app.js
+++ myExpressApp/app.js
@@ -29,13 +29,18 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// =============================
+// Listen on Azure port
+// =============================
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
 });
 
 module.exports = app;
